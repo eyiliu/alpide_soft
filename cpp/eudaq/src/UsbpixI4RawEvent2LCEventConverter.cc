4b9de515e52f7a207d7de166ed5381a4446cbb94
Yi Liu <yi.liu@desy.de>
2019-03-13 18:15:35 +0100

    default debug build. TODO: unknown runtime issue in USBPIX lcio converter by Release build

--- cpp/eudaq/src/UsbpixI4RawEvent2LCEventConverter.cc
+++ cpp/eudaq/src/UsbpixI4RawEvent2LCEventConverter.cc
@@ -4,7 +4,6 @@
 #include "eudaq/StandardEvent.hh"
 #include "eudaq/Logger.hh"
 
-
 #include "IMPL/TrackerRawDataImpl.h"
 #include "IMPL/TrackerDataImpl.h"
 #include "UTIL/CellIDEncoder.h"
@@ -13,138 +12,101 @@
 #include <cstring>
 #include <exception>
 #include <bitset>
-/*FEI4A
- *  DATA_HEADER_LV1ID_MASK	0x00007F00
- *  DATA_HEADER_BCID_MASK	0x000000FF
- *
- *FEI4B
- *  DATA_HEADER_LV1ID_MASK	0x00007C00
- *  DATA_HEADER_BCID_MASK	0x000003FF
- */
 
-typedef unsigned int uint;
-template <uint dh_lv1id_msk, uint dh_bcid_msk> class ATLASFEI4Interpreter {
+//////////////////bit order
+#ifdef _WIN32
+#include <crtdefs.h>
+#define BE16TOH ntohs
+#define BE32TOH ntohl
+#define BE64TOH ntohll
+#define LE16TOH
+#define LE32TOH
+#define LE64TOH
+#else
+
+#ifdef __APPLE__
+#include <machine/endian.h>
+#if __DARWIN_BYTE_ORDER == __DARWIN_LITTLE_ENDIAN
+#define BE16TOH NTOHS
+#define BE32TOH NTOHL
+#define BE64TOH NTOHLL
+#define LE16TOH
+#define LE32TOH
+#define LE64TOH
+#endif
+#else
+
+#include <endian.h>
+#define BE16TOH be16toh
+#define BE32TOH be32toh
+#define BE64TOH be64toh
+#define LE16TOH le16toh
+#define LE32TOH le32toh
+#define LE64TOH le64toh
+#endif
+#endif
+///////////////////////
+
+class ATLASFEI4Interpreter {
 public:
   //-----------------
   // Data Header (dh)
   //-----------------
-  static const uint dh_wrd = 0x00E90000;
-  static const uint dh_msk = 0xFFFF0000;
-  static const uint dh_flag_msk = 0x00008000;
-
-  inline bool is_dh(uint X) const { return (dh_msk & X) == dh_wrd; }
-  inline uint get_dh_flag(uint X) const { return (dh_flag_msk & X) >> 15; }
-  inline bool is_dh_flag_set(uint X) const {
-    return (dh_flag_msk & X) == dh_flag_msk;
-  }
-  //	inline uint get_dh_lv1id(uint X) const		{return (dh_lv1id_msk & X) >>
-  //determineShift(dh_lv1id_msk);}
-  inline uint get_dh_lv1id(uint X) const {
-    return (dh_lv1id_msk & X) >> myBitMask<dh_lv1id_msk>::shiftValue;
-  }
-  inline uint get_dh_bcid(uint X) const { return (dh_bcid_msk & X); }
+  static const uint32_t dh_wrd = 0x00E90000;
+  static const uint32_t dh_msk = 0xFFFF0000;
+  static const uint32_t dh_flag_msk = 0x00008000;
 
   //-----------------
   // Data Record (dr)
   //-----------------
-  static const uint dr_col_msk = 0x00FE0000;
-  static const uint dr_row_msk = 0x0001FF00;
-  static const uint dr_tot1_msk = 0x000000F0;
-  static const uint dr_tot2_msk = 0x0000000F;
+  static const uint32_t dr_col_msk = 0x00FE0000;
+  static const uint32_t dr_row_msk = 0x0001FF00;
+  static const uint32_t dr_tot1_msk = 0x000000F0;
+  static const uint32_t dr_tot2_msk = 0x0000000F;
 
   // limits of FE-size
-  static const uint rd_min_col = 1;
-  static const uint rd_max_col = 80;
-  static const uint rd_min_row = 1;
-  static const uint rd_max_row = 336;
+  static const uint32_t rd_min_col = 1;
+  static const uint32_t rd_max_col = 80;
+  static const uint32_t rd_min_row = 1;
+  static const uint32_t rd_max_row = 336;
 
   // the limits shifted for easy verification with a dr
-  static const uint dr_min_col = rd_min_col << 17;
-  static const uint dr_max_col = rd_max_col << 17;
-  static const uint dr_min_row = rd_min_row << 8;
-  static const uint dr_max_row = rd_max_row << 8;
+  static const uint32_t dr_min_col = rd_min_col << 17;
+  static const uint32_t dr_max_col = rd_max_col << 17;
+  static const uint32_t dr_min_row = rd_min_row << 8;
+  static const uint32_t dr_max_row = rd_max_row << 8;
 
-  inline bool is_dr(uint X) const {
-    // check if hit is within limits of FE size
+  //-----------------
+  // Trigger Data (tr)
+  //-----------------
+  static const uint32_t tr_wrd_hdr_v10 = 0x00FFFF00;
+  static const uint32_t tr_wrd_hdr_msk_v10 = 0xFFFFFF00;
+  static const uint32_t tr_wrd_hdr = 0x00F80000; // tolerant to 1-bit flips and
+  // not equal to control/comma
+  // symbols
+  static const uint32_t tr_wrd_hdr_msk = 0xFFFF0000;
+
+  static const uint32_t tr_no_31_24_msk = 0x000000FF;
+  static const uint32_t tr_no_23_0_msk = 0x00FFFFFF;
+
+  static const uint32_t tr_data_msk = 0x0000FF00; // trigger error + trigger mode
+  static const uint32_t tr_mode_msk = 0x0000E000; // trigger mode
+  static const uint32_t tr_err_msk = 0x00001F00;  // error code: bit 0: wrong
+
+  static bool is_dh(uint32_t X) { return (dh_msk & X) == dh_wrd; }  
+  static bool is_dr(uint32_t X) {
     return (((dr_col_msk & X) <= dr_max_col) &&
 	    ((dr_col_msk & X) >= dr_min_col) &&
 	    ((dr_row_msk & X) <= dr_max_row) &&
 	    ((dr_row_msk & X) >= dr_min_row));
   }
-
-  inline uint get_dr_col1(uint X) const { return (dr_col_msk & X) >> 17; }
-  inline uint get_dr_row1(uint X) const { return (dr_row_msk & X) >> 8; }
-  inline uint get_dr_tot1(uint X) const { return (dr_tot1_msk & X) >> 4; }
-  inline uint get_dr_col2(uint X) const { return (dr_col_msk & X) >> 17; }
-  inline uint get_dr_row2(uint X) const {
-    return ((dr_row_msk & X) >> 8) + 1;
-  }
-  inline uint get_dr_tot2(uint X) const { return (dr_tot2_msk & X); }
-
-  //-----------------
-  // Trigger Data (tr)
-  //-----------------
-  static const uint tr_wrd_hdr_v10 = 0x00FFFF00;
-  static const uint tr_wrd_hdr_msk_v10 = 0xFFFFFF00;
-  static const uint tr_wrd_hdr = 0x00F80000; // tolerant to 1-bit flips and
-  // not equal to control/comma
-  // symbols
-  static const uint tr_wrd_hdr_msk = 0xFFFF0000;
-
-  static const uint tr_no_31_24_msk = 0x000000FF;
-  static const uint tr_no_23_0_msk = 0x00FFFFFF;
-
-  static const uint tr_data_msk = 0x0000FF00; // trigger error + trigger mode
-  static const uint tr_mode_msk = 0x0000E000; // trigger mode
-  static const uint tr_err_msk = 0x00001F00;  // error code: bit 0: wrong
-  // number of dh, bit 1 service
-  // record recieved
-
-  inline bool is_tr(uint X) const {
-    return ((tr_wrd_hdr_msk & X) == tr_wrd_hdr) ||
-      ((tr_wrd_hdr_msk_v10 & X) == tr_wrd_hdr_v10);
-  }
-
-  inline uint get_tr_no_2(uint X, uint Y) const {
-    return ((tr_no_31_24_msk & X) << 24) | (tr_no_23_0_msk & Y);
-  }
-  inline bool get_tr_err_occurred(uint X) const {
-    return (((tr_err_msk & X) >> 8) == 0x0) ||
-      ((tr_wrd_hdr_msk_v10 & X) == tr_wrd_hdr_v10);
-  }
-
-  inline uint get_tr_data(uint X) const { return (tr_data_msk & X) >> 8; }
-  inline uint get_tr_err(uint X) const { return (tr_err_msk & X) >> 8; }
-  inline uint get_tr_mode(uint X) const { return (tr_mode_msk & X) >> 13; }
-
-  // determine necessary bitshift from a bitmask
-  /*	constexpr uint determineShift(uint mask) const
-	{
-	uint count = 0;
-	std::bitset<32> maskField(mask);
-
-	while(maskField[count] != true)
-	{
-	count++;
-	}
-	return count;
-	}
-  */
-  template <uint bitmask> struct myBitMask {
-    static uint getShift() {
-      uint count = 0;
-      std::bitset<32> maskField(bitmask);
-
-      while (maskField[count] != true) {
-	count++;
-      }
-      return count;
-    }
-
-    enum { shiftValue = getShift() };
-  };
-
-}; // class ATLASFEI4Interpreter
+  static uint32_t get_dr_col1(uint32_t X) { return (dr_col_msk & X) >> 17; }
+  static uint32_t get_dr_row1(uint32_t X) { return (dr_row_msk & X) >> 8; }
+  static uint32_t get_dr_tot1(uint32_t X) { return (dr_tot1_msk & X) >> 4; }
+  static uint32_t get_dr_col2(uint32_t X) { return (dr_col_msk & X) >> 17; }
+  static uint32_t get_dr_row2(uint32_t X) { return ((dr_row_msk & X) >> 8) + 1;}
+  static uint32_t get_dr_tot2(uint32_t X) { return (dr_tot2_msk & X); }
+};
 
 class UsbpixrefRawEvent2LCEventConverter: public eudaq::LCEventConverter{
   typedef std::vector<uint8_t>::const_iterator datait;
@@ -153,14 +115,8 @@ public:
 
   static const uint32_t m_id_factory = eudaq::cstr2hash("USBPIXI4");
 private:
-
-  uint32_t getWord(const std::vector<uint8_t>& data, size_t index) const;
-  eudaq::StandardPlane ConvertPlane(const std::vector<uint8_t> & data, uint32_t id) const;
-  bool isEventValid(const std::vector<uint8_t> & data) const;
   bool getHitData(uint32_t &Word, bool second_hit,
 		  uint32_t &Col, uint32_t &Row, uint32_t &ToT) const;
-  uint32_t getTrigger(const std::vector<uint8_t> & data) const;
-
   static const uint32_t CHIP_MIN_COL = 1;
   static const uint32_t CHIP_MAX_COL = 80;
   static const uint32_t CHIP_MIN_ROW = 1;
@@ -168,10 +124,6 @@ private:
   static const uint32_t CHIP_MAX_ROW_NORM = CHIP_MAX_ROW - CHIP_MIN_ROW;
   static const uint32_t CHIP_MAX_COL_NORM = CHIP_MAX_COL - CHIP_MIN_COL;
   uint32_t consecutive_lvl1 = 16;
-  
-  static ATLASFEI4Interpreter<0x00007F00, 0x000000FF> fei4a_intp;
-
-
   uint32_t first_sensor_id = 0;
   uint32_t chip_id_offset = 10;
 };
@@ -181,39 +133,30 @@ namespace{
     Register<UsbpixrefRawEvent2LCEventConverter>(UsbpixrefRawEvent2LCEventConverter::m_id_factory);
 }
 
-ATLASFEI4Interpreter<0x00007F00, 0x000000FF> UsbpixrefRawEvent2LCEventConverter::fei4a_intp;
-
 bool UsbpixrefRawEvent2LCEventConverter::
 Converting(eudaq::EventSPC d1, eudaq::LCEventSP d2, eudaq::ConfigurationSPC conf)const {
-  auto& lcioEvent = *(d2.get());
-  lcioEvent.parameters().setValue("EventType",2); //( eutelescope::EUTELESCOPE::EVENTTYPE, eutelescope::kDE )
-  //pointer to collection which will store data
-  lcio::LCCollectionVec * zsDataCollection;
-  //it can be already in event or has to be created
-  bool zsDataCollectionExists = false;
-  try{
-    zsDataCollection = static_cast< lcio::LCCollectionVec* > ( lcioEvent.getCollection( "zsdata_apix" ) );
-    zsDataCollectionExists = true;
+  auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
+  d2->parameters().setValue("EventType",2);
+  lcio::LCCollectionVec * zsDataCollection = nullptr;
+  auto p_col_names = d2->getCollectionNames();
+  if(std::find(p_col_names->begin(), p_col_names->end(), "zsdata_apix") != p_col_names->end()){
+    zsDataCollection = dynamic_cast<lcio::LCCollectionVec*>(d2->getCollection("zsdata_alpide"));
+    if(!zsDataCollection)
+      EUDAQ_THROW("dynamic_cast error: from lcio::LCCollection to lcio::LCCollectionVec");
   }
-  catch( lcio::DataNotAvailableException& e ){
-    zsDataCollection = new lcio::LCCollectionVec( lcio::LCIO::TRACKERDATA );
+  else{
+    zsDataCollection = new lcio::LCCollectionVec(lcio::LCIO::TRACKERDATA);
+    d2->addCollection(zsDataCollection, "zsdata_apix");
   }
-
-  //create cell encoders to set sensorID and pixel type
   lcio::CellIDEncoder<lcio::TrackerDataImpl> zsDataEncoder("sensorID:7,sparsePixelType:5", zsDataCollection);
-  //( eutelescope::EUTELESCOPE::ZSDATADEFAULTENCODING, zsDataCollection  )
-
-  //this is an event as we sent from Producer, needs to be converted to concrete type RawEvent
-  auto ev_raw = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
-
-  auto block_n_list = ev_raw->GetBlockNumList();
+  auto block_n_list = ev->GetBlockNumList();
   for(auto &chip: block_n_list){
-    auto buffer = ev_raw->GetBlock(chip);
-    int sensorID  = chip + chip_id_offset + first_sensor_id;
-
+    auto rawblock = ev->GetBlock(chip);
+    uint32_t* buffer = reinterpret_cast<uint32_t*>(rawblock.data());
+    size_t buffer_size = rawblock.size()/sizeof(uint32_t);
+    
     lcio::TrackerDataImpl *zsFrame = new lcio::TrackerDataImpl;
-
-    zsDataEncoder["sensorID"] = sensorID;
+    zsDataEncoder["sensorID"] = chip + chip_id_offset + first_sensor_id;;
     zsDataEncoder["sparsePixelType"] = 2;//eutelescope::kEUTelGenericSparsePixel
     zsDataEncoder.setCellID(zsFrame);
 
@@ -222,64 +165,41 @@ Converting(eudaq::EventSPC d1, eudaq::LCEventSP d2, eudaq::ConfigurationSPC conf
     uint32_t Row = 0;
     uint32_t lvl1 = 0;
 
-    for(size_t i=0; i < buffer.size()-4; i += 4){
-      uint32_t Word = getWord(buffer, i);
-      if(fei4a_intp.is_dh(Word)){
-	lvl1++;
+    for(size_t i=0; i < buffer_size; i ++){
+      uint32_t Word = LE32TOH(buffer[i]);
+      if(ATLASFEI4Interpreter::is_dh(Word)){
+  	lvl1++;
       }
       else{
-	if(lvl1==0){
-	  //std::cout<< "fei4 event might be invalid, skipped"<<std::endl;
-	  break; //hacking, TODO: otherwise levl1 - 1  = -1, not acceptable!
-	}
-
-	//First Hit
-	if(getHitData(Word, false, Col, Row, ToT)){
-	  zsFrame->chargeValues().push_back(335-Row);//x
-	  zsFrame->chargeValues().push_back(Col);//y
-	  zsFrame->chargeValues().push_back(ToT);//signal
-	  zsFrame->chargeValues().push_back(lvl1-1);//time
-	}
-	//Second Hit
-	if(getHitData(Word, true, Col, Row, ToT)){
-	  zsFrame->chargeValues().push_back(335-Row);
-	  zsFrame->chargeValues().push_back(Col);
-	  zsFrame->chargeValues().push_back(ToT);
-	  zsFrame->chargeValues().push_back(lvl1-1);
-	}
+  	if(lvl1==0){
+  	  //std::cout<< "fei4 event might be invalid, skipped"<<std::endl;
+  	  break; //hacking, TODO: otherwise levl1 - 1  = -1, not acceptable!
+  	}
+  	//First Hit
+  	if(getHitData(Word, false, Col, Row, ToT)){
+  	  zsFrame->chargeValues().push_back(335-Row);//x
+  	  zsFrame->chargeValues().push_back(Col);//y
+  	  zsFrame->chargeValues().push_back(ToT);//signal
+  	  zsFrame->chargeValues().push_back(lvl1-1);//time
+  	}
+  	//Second Hit
+  	if(getHitData(Word, true, Col, Row, ToT)){
+  	  zsFrame->chargeValues().push_back(335-Row);
+  	  zsFrame->chargeValues().push_back(Col);
+  	  zsFrame->chargeValues().push_back(ToT);
+  	  zsFrame->chargeValues().push_back(lvl1-1);
+  	}
       }
     }
     zsDataCollection->push_back( zsFrame);
   }
-  if((!zsDataCollectionExists)  && ( zsDataCollection->size() != 0 ) ){
-    lcioEvent.addCollection( zsDataCollection, "zsdata_apix" );
-  }
 }
-
-bool UsbpixrefRawEvent2LCEventConverter::
-isEventValid(const std::vector<uint8_t> & data) const{
-  //ceck data consistency
-  uint32_t dh_found = 0;
-  for (size_t i=0; i < data.size()-8; i += 4){
-    uint32_t word = getWord(data, i);
-    if(fei4a_intp.is_dh(word)){
-      dh_found++;
-    }
-  }
-  if(dh_found != consecutive_lvl1){
-    return false;
-  }
-  else{
-    return true;
-  }
-}
-
 
 bool UsbpixrefRawEvent2LCEventConverter::
 getHitData(uint32_t &Word, bool second_hit,
 	   uint32_t &Col, uint32_t &Row, uint32_t &ToT) const{
   //No data record
-  if( !fei4a_intp.is_dr(Word)){
+  if( !ATLASFEI4Interpreter::is_dr(Word)){
     return false;
   }
   uint32_t t_Col=0;
@@ -287,14 +207,14 @@ getHitData(uint32_t &Word, bool second_hit,
   uint32_t t_ToT=15;
 
   if(!second_hit){
-    t_ToT = fei4a_intp.get_dr_tot1(Word);
-    t_Col = fei4a_intp.get_dr_col1(Word);
-    t_Row = fei4a_intp.get_dr_row1(Word);
+    t_ToT = ATLASFEI4Interpreter::get_dr_tot1(Word);
+    t_Col = ATLASFEI4Interpreter::get_dr_col1(Word);
+    t_Row = ATLASFEI4Interpreter::get_dr_row1(Word);
   }
   else{
-    t_ToT = fei4a_intp.get_dr_tot2(Word);
-    t_Col = fei4a_intp.get_dr_col2(Word);
-    t_Row = fei4a_intp.get_dr_row2(Word);
+    t_ToT = ATLASFEI4Interpreter::get_dr_tot2(Word);
+    t_Col = ATLASFEI4Interpreter::get_dr_col2(Word);
+    t_Row = ATLASFEI4Interpreter::get_dr_row2(Word);
   }
 
   //translate FE-I4 ToT code into tot
@@ -317,24 +237,4 @@ getHitData(uint32_t &Word, bool second_hit,
   Col = t_Col;
   Row = t_Row;
   return true;
-}
-
-uint32_t UsbpixrefRawEvent2LCEventConverter::
-getTrigger(const std::vector<uint8_t> & data) const{
-  //Get Trigger Number and check for errors
-  uint32_t i = data.size() - 8; //splitted in 2x 32bit words
-  uint32_t Trigger_word1 = getWord(data, i);
-
-  if(Trigger_word1==(uint32_t) -1){
-    return (uint32_t)-1;
-  }
-  uint32_t Trigger_word2 = getWord(data, i+4);
-  uint32_t trigger_number = fei4a_intp.get_tr_no_2(Trigger_word1, Trigger_word2);
-  return trigger_number;
-}
-
-uint32_t UsbpixrefRawEvent2LCEventConverter::
-getWord(const std::vector<uint8_t>& data, size_t index) const{
-  return (((uint32_t)data[index + 3]) << 24) | (((uint32_t)data[index + 2]) << 16)
-    | (((uint32_t)data[index + 1]) << 8) | (uint32_t)data[index];
 }
