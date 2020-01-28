7f95f997207128a8a3884c54743d8d4e7a6d0fff
Yi Liu <yi.liu@desy.de>
2020-01-28 08:28:59 +0100

    clean up

--- cpp/altel/register/mount-origin/colorblocks.js
+++ cpp/altel/register/mount-origin/colorblocks.js
@@ -1,4 +1,3 @@
-
 // matrix size
 let pixelNumberX = 1024;
 let pixelNumberY = 512;
@@ -11,7 +10,7 @@ let scalerFactorY = 4;
 let cellNumberX = Math.ceil(pixelNumberX/scalerFactorX);
 let cellNumberY = Math.ceil(pixelNumberY/scalerFactorY);
 let cellNumber = cellNumberX * cellNumberY;
-    
+
 let groupCellNumberX = Math.ceil(groupPixelNumberX/scalerFactorX);
 let groupCellNumberY = Math.ceil(groupPixelNumberY/scalerFactorY);
 let groupNumberX = Math.ceil(cellNumberX / groupCellNumberX);
@@ -32,7 +31,7 @@ let data = [];
 let trigger_n = 0
 function getEventArray(){
     let ev_array = new Array();
-    let n_ev = Math.floor(Math.random() * 5);
+    let n_ev = Math.floor(Math.random() * 10);
     for(let i = 0; i< n_ev; i++){
         trigger_n ++;
         let ev = {detector_type:"alpide", trigger: trigger_n, ext: 1, geometry:[1024, 512, 6], data_type: "hit_xyz_array"};
@@ -52,29 +51,34 @@ function getEventArray(){
 //
 
 // global width, height
-function draw(canvas, custom) {
+function updateCanvas(canvas, custom) {
     let context = canvas.node().getContext('2d');
-    context.clearRect(0, 0, width, height);
-    let elements = custom.selectAll('custom.rect');
-    elements.each(function(d,i) {
+    context.clearRect(0, 0, canvas.width, canvas.height);  //clear full canvas
+    let u = custom.selectAll('custom.rect');
+    u.each(function(d,i) {
 	let node = d3.select(this);
 	context.fillStyle =  node.attr('fillStyle');
 	context.fillRect(node.attr('x'), node.attr('y'), node.attr('width'), node.attr('height'))
+        node = null;
     });
 }
 
-
-// === Bind and draw functions === //
+// 
 function databind(data, custom) {    
     let colorScale = d3.scaleSequential()
         .domain([0, 256])
         .interpolator(d3.interpolatePlasma);
 
-    let join = custom.selectAll('custom.rect').data(data);
-    let enterSel = join.enter()
+    // https://www.d3indepth.com/enterexit/
+    let u = custom
+        .selectAll('custom.rect')
+        .data(data);
+
+    u.enter()
 	.append('custom')
-	.attr('class', 'rect')
-        .attr('id', function(d){return "cell" + d.id;} )
+        .merge(u)
+        .transition()
+    	.attr('class', 'rect')
 	.attr('x', function(d, i) {
             let x1 = Math.floor(i % cellNumberX);
             let xg = Math.floor(x1 / groupCellNumberX);
@@ -85,41 +89,35 @@ function databind(data, custom) {
             let yg = Math.floor(y1 / groupCellNumberY);
             return (cellSpacing + cellSize) * y1 + groupSpacingY * (yg+1);
         })
-        .attr('width', 0)
-        .attr('height', 0)
-    ;
-
-    join.merge(enterSel)
-        .transition()
         .attr('width', cellSize)
         .attr('height', cellSize)
-        .attr('fillStyle', function(d) { return colorScale(d.hit_count); })
-
-    let exitSel = join.exit()
+        .attr('fillStyle', function(d) { return colorScale(d.hit_count); }  )
+    
+    u.exit()
         .transition()
         .attr('width', 0)
         .attr('height', 0)
         .remove();
 }
 
-
 function DOMContentLoadedListener_colorblocks() {
     for( let i = 0; i<cellNumber; i++ ){
-        data.push( { hit_count: 0, id:i}   );
+        data.push( { hit_count: 0, hit_count_old: 0}   );
     }
     
     let customBase = document.createElement('custom');
     let custom = d3.select(customBase); // this is our svg replacement
 
-    let mainCanvas = d3.select('#container')
-    .append('canvas')
-    .classed('mainCanvas', true)
-    .attr('width', width)
-    .attr('height', height);
+    let mainCanvas = d3.select('#container0')
+        .append('canvas')
+        .classed('mainCanvas', true)
+        .attr('width', width)
+        .attr('height', height);
     
     dummyEventTimer();
     function dummyEventTimer(){
         let ev_array = getEventArray();
+        //UpdateData
         let n_ev =  ev_array.length;
         for(let i=0; i< n_ev; i++ ){
             let ev = ev_array[i];
@@ -132,14 +130,18 @@ function DOMContentLoadedListener_colorblocks() {
                 cellX=Math.floor(pixelX/scalerFactorX);
                 cellY=Math.floor(pixelY/scalerFactorY);
                 cellN= cellX + cellNumberX * cellY;
+                data[cellN].hit_count_old =  data[cellN].hit_count;
                 data[cellN].hit_count = 255;
             }
         }
         databind(data, custom);
-        // console.log("callling");
-        draw(mainCanvas, custom);
-        // console.log("called");
-        setTimeout(dummyEventTimer, 1000);
+        setTimeout(dummyEventTimer, 100);
+    }
+
+    updateCanvasTimer();
+    function updateCanvasTimer(){
+        updateCanvas(mainCanvas, custom);
+        setTimeout(updateCanvasTimer, 1000);
     }
 }
 
--- cpp/altel/register/mount-origin/example.js
+++ cpp/altel/register/mount-origin/example.js
@@ -1,92 +1,75 @@
-var head = 0, tail = 0, ring = new Array();
 
-function get_appropriate_ws_url(extra_url)
-{
+let alive = 0;
+
+function get_appropriate_ws_url(extra_url){
     let pcol;
     let u = document.URL;
-
-    /*
-     * We open the websocket encrypted if this page came on an
-     * https:// url itself, otherwise unencrypted
-     */
-
     if (u.substring(0, 5) === "https") {
 	pcol = "wss://";
 	u = u.substr(8);
-    } else {
+    }
+    else {
 	pcol = "ws://";
 	if (u.substring(0, 4) === "http")
 	    u = u.substr(7);
     }
-
     u = u.split("/");
-
-    /* + "/xxx" bit is for IE10 workaround */
-
     return pcol + u[0] + "/" + extra_url;
 }
 
-function new_ws(urlpath, protocol)
-{
+function new_ws(urlpath, protocol){
     if (typeof MozWebSocket != "undefined")
 	return new MozWebSocket(urlpath, protocol);
-
     return new WebSocket(urlpath, protocol);
 }
 
+function on_ws_open() {
+    document.getElementById("r").disabled = 0;
+    alive++;
+};
+
+function on_ws_close(){
+    alive --;
+    if (alive === 0)
+	document.getElementById("r").disabled = 1;
+};
 
 function DOMContentLoadedListener() {
-    let n, wsa = new Array, alive = 0;    
-    for (n = 0; n < 1; n++) {
-	
-	ws = new_ws(get_appropriate_ws_url(""), "lws-minimal");
-	wsa.push(ws);
-	try {
-	    ws.onopen = function() {
-		document.getElementById("r").disabled = 0;
-		alive++;
-	    };
-
-	    ws.onmessage = function got_packet(msg) {
-		let n, s = "";
-		
-		ring[head] = msg.data + "\n";
-		head = (head + 1) % 50;
-		if (tail === head)
-		    tail = (tail + 1) % 50;
-		
-		n = tail;
-		do {
-		    s = s + ring[n];
-		    n = (n + 1) % 50;
-		} while (n !== head);
-		
-		document.getElementById("r").value = s; 
-		document.getElementById("r").scrollTop =
-		    document.getElementById("r").scrollHeight;
-	    };
-
-	    ws.onclose = function(){
-		if (--alive === 0)
-		    document.getElementById("r").disabled = 1;
-	    };
-
-            
-            function sendmsg()
-	    {
-		ws.send(document.getElementById("m").value);
-		document.getElementById("m").value = "";
-	    }
-	    
-	    document.getElementById("b").addEventListener("click", sendmsg);
-            
-            
-	} catch(exception) {
-	    alert("<p>Error " + exception);  
-	}
-
+    let wsa = [];
+    ws = new_ws(get_appropriate_ws_url(""), "lws-minimal");
+    wsa.push(ws);
+    ws.onopen = on_ws_open;
+    ws.onclose = on_ws_close;
         
+    let head = 0;
+    let tail = 0;
+    let ring = [];
+    ws.onmessage = function got_packet(msg) {
+	let n;
+        let s = "";
+	
+	ring[head] = msg.data + "\n";
+	head = (head + 1) % 50;
+	if (tail === head)
+	    tail = (tail + 1) % 50;
+	
+	n = tail;
+	do {
+	    s = s + ring[n];
+	    n = (n + 1) % 50;
+	} while (n !== head);
+	
+	document.getElementById("r").value = s; 
+	document.getElementById("r").scrollTop =
+	    document.getElementById("r").scrollHeight;
+    };
+    
+    function sendmsg(){
+	ws.send(document.getElementById("m").value);
+	document.getElementById("m").value = "";
     }
+    
+    document.getElementById("b").addEventListener("click", sendmsg);   
 }
 
 
